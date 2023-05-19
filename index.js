1bcffdd099ad40c3fbb16d8d5774691b16edd362
imran-h-ob <imran.h.ovi.61@gmail.com>
2023-05-19 17:30:10 +0600

    server and database connection complete

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-require("dotenv").config();
 
+require("dotenv").config();
 const app = express();
 
 app.use(cors());
@@ -10,7 +10,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.jwgax72.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -18,26 +17,23 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const toyData = client.db("toyDB").collection("toys");
-
+    const toyCollections = client.db("toyDB").collection("toys");
     app.get("/toys", async (req, res) => {
-      const getToyData = toyData.find();
-      const result = await getToyData.toArray();
+      const getAllToys = toyCollections.find({});
+      const result = await getAllToys.toArray();
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
@@ -47,8 +43,6 @@ app.get("/", (req, res) => {
   res.send("app is running");
 });
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`app is listening from port ${port}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`app is listening from ${process.env.PORT || 5000}`);
 });
